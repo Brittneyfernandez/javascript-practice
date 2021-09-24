797bac69a2a577f1c2c0dba58ae5aa53befeb314
Brittney Fernandez <bltorres1990@gmail.com>
2021-09-23 17:15:30 -0700

    takehome-1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const appName = "My Cool App";
+
+
+const no = false;
+
+// primitve data types
+
+const yes = true;
+const number = 1.27;
+const string = 'abc';
+
+// organize our data types
+
+const array={
+    
+}
+
+/*
+mulit-line comment
+*/
+console.log('Brittney');
+console.log('Tacos');
