ebb0c5ca50eaca631c4b932f3506dfe89f6e4314
Brittney Fernandez <bltorres1990@gmail.com>
2021-09-23 19:37:25 -0700

    numbers

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log('Supercalifragilisticexpialidocious' );
+console.log('Supercalifragilisticexpialidocious'.length );
+console.log(50);
+console.log(31n);
+console.log(typeof 50);
+console.log(typeof 31n);
